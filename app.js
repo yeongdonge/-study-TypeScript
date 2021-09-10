3366b8f673e708aefbfe5014b5ad9b9c7ecf954f
yeongdonge <parrineau@gmail.com>
2021-09-10 21:08:40 +0900

    feat: Typescrpit Watch 모드, 타입 명시 실습

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-var age = 10;
+var studentID = 12345;
+var stduentName = "Jenny Kim";
+var age = 21;
+var gender = "female";
+var subject = "Javascript";
+var courseCompleted = false;
+function getStudentDetails(stduentID) {
+    return null;
+}
--- app.ts
+++ app.ts
@@ -1,1 +1,17 @@
-let age:number = 10;
+let studentID:number = 12345;
+let stduentName:string = "Jenny Kim";
+let age:number = 21;
+let gender:string = "female";
+let subject:string = "Javascript";
+let courseCompleted:boolean = false;
+
+function getStudentDetails(stduentID: number):{
+    studentID: number;
+    studentName: string;
+    age: number;
+    gender: string;
+    subject: string;
+    createDate: Date;
+} {
+    return null;
+}
